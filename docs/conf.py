e1e882c37a0aa909a7226aef24244ef1163ee437
nancy291013 <rakeshgupta212188@gmail.com>
2025-06-19 14:14:30 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,11 @@
+project = 'Avast'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
 
